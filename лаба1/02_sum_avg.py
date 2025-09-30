2a2acbe52f2266e98da36427def75d202018c331
ulyanamalik <malikova.u@list.ru>
2025-09-30 09:41:00 +0300

    Update 02_sum_avg.py

--- лаба1/02_sum_avg.py
+++ лаба1/02_sum_avg.py
@@ -1,6 +1,11 @@
-a=input('первое число:')
-b=input('второе число:')
-if ',' in a or ','in b:
-    a=a.replace(',','.')
-    b=b.replace(',','.')
-print(f'sum = {float(a)+float(b)}; avg = {(float(a)+float(b))/2}')
+a = input("Введите первое число: ")
+b = input("Введите второе число: ")
+
+if ',' in a or ',' in b:
+    a = a.replace(",", ".")
+    b = b.replace(",", ".")
+
+sum_result = float(a) + float(b)
+avg_result = sum_result / 2
+
+print(f'sum = {sum_result:.2f}; avg = {avg_result:.2f}')
